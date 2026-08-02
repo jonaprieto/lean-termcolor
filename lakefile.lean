dc1f73ed37708ba3d1c552129023401499bf2c6b
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-02 15:31:05 -0500

    chore(release): set version 1.0.1

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «termcolor» where
-  version := v!"1.0.0"
+  version := v!"1.0.1"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 @[default_target]
